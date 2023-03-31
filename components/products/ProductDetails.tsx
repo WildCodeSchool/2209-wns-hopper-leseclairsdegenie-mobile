133289279afc177792abf62ad8c40ae44b2c5705
Oscar-AGUDELO <oscar.agudelo.pro@gmail.com>
2023-03-31 15:44:21 +0200

    products + components

--- components/products/ProductDetails.tsx
+++ components/products/ProductDetails.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { Image, View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
 import { IProduct } from "../../interfaces";
 import Ionicons from "@expo/vector-icons/Ionicons";
+const { width, height } = Dimensions.get("window");
 
 interface IProductDetailsProps {
   product: IProduct | null;
@@ -16,28 +24,33 @@ export function ProductDetails({
 }: IProductDetailsProps) {
   if (product !== null && product.disponibility) {
     return (
-      <View key={product.id}>
+      <View key={product.id} style={styles.container}>
+        <View style={styles.backgroundF} />
         <TouchableOpacity
           onPress={() => {
             closeProductDetails();
           }}
+          style={styles.closeButton}
         >
-          <Image style={styles.tinyLogo} source={{ uri: product.image }} />
+          <Text style={styles.closeText}>x</Text>
         </TouchableOpacity>
-        <View>
-          <Text>{product.name}</Text>
-        </View>
-        <View>
-          <View>
-            <Text>{product.price}€ / jour</Text>
+        <View style={styles.containerContain}>
+          <View style={styles.containerContain1}>
+            <Image style={styles.tinyLogo} source={{ uri: product.image }} />
+            <View style={styles.containerContain2}>
+              <Text style={styles.containerName}>{product.name}</Text>
+              <Text style={styles.containerPrice}>{product.price}€ / jour</Text>
+            </View>
           </View>
+          <Text style={styles.description}>{product.description}</Text>
           <TouchableOpacity
             onPress={() => {
               addOnBasket(product);
             }}
+            style={styles.addButton}
           >
-            <Text>+</Text>
-            <Ionicons name="basket-outline" />
+            <Text style={styles.addIcon}>+</Text>
+            <Ionicons name="basket-outline" style={styles.cartIcon} />
           </TouchableOpacity>
         </View>
       </View>
@@ -47,16 +60,84 @@ export function ProductDetails({
   }
 }
 
+const white = "#ffffff";
 const styles = StyleSheet.create({
+  backgroundF: {
+    height: height,
+    backgroundColor: "black",
+    opacity: 0.3,
+    zIndex: 0,
+    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+  },
+  closeButton: {
+    zIndex: 1,
+    position: "absolute",
+    right: 15,
+    top: 15,
+  },
+  closeText: {
+    fontSize: 30,
+    borderRadius: 500,
+    backgroundColor: white,
+    color: "black",
+    height: 35,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 35,
+    lineHeight: 33,
+  },
   container: {
-    paddingTop: 50,
+    alignItems: "center",
+    height: height,
+    width: width,
+    justifyContent: "center",
+  },
+  containerContain: {
+    backgroundColor: "#FFFBFE",
+    borderRadius: 15,
+    height: 500,
+    width: "80%",
+    padding: 10,
+  },
+  containerContain1: {
+    height: 370,
+    alignIjustifyContenttems: "center",
   },
   tinyLogo: {
-    width: 50,
+    width: "100%",
+    height: 320,
+  },
+  containerContain2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     height: 50,
   },
-  logo: {
-    width: 66,
-    height: 58,
+  containerName: { fontSize: 15, fontWeight: "500" },
+  containerPrice: { fontSize: 13, fontWeight: "500" },
+  description: {
+    height: 80,
+  },
+  addButton: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginLeft: 5,
+  },
+  addIcon: {
+    borderRadius: 500,
+    backgroundColor: "black",
+    color: white,
+    height: 15,
+    width: 15,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    lineHeight: 16,
+  },
+  cartIcon: {
+    fontSize: 30,
   },
 });
--- screens/Products.tsx
+++ screens/Products.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Modal,
   Text,
@@ -10,23 +10,51 @@ import {
   TextInput,
   SafeAreaView,
   FlatList,
+  ScrollView,
+  Button,
+  Keyboard,
 } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { IProduct } from "../interfaces";
+import { ICategory, IProduct } from "../interfaces";
 import { getProducts } from "../graphql/productQueries";
 import { ProductCard } from "../components/products/ProductCard";
 import { ProductDetails } from "../components/products/ProductDetails";
+import { MainContext } from "../MainContexts";
 const { width, height } = Dimensions.get("window");
 export function Products() {
+  const Main = useContext(MainContext);
+  const { categorie, setCategorie, categories } = Main;
   const [openModalProduct, setOpenModalProduct] = useState(false);
+  const [openCategories, setOpenCategories] = useState(false);
   const [researchInput, setResearchInput] = useState("");
-  const [categorie, setCategorie] = useState("Montagne");
+  const [researchOn, setResearchOn] = useState(false);
   const [researchResult, setResearchResult] = useState([]);
   const [productToOpen, setProductToOpen] = useState<IProduct>();
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setKeyboardVisible(true); // or some other action
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setKeyboardVisible(false); // or some other action
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
   const { loading, data, refetch } = useQuery<{ products: IProduct[] }>(
     getProducts
   );
-  const products = data ? data.products.concat(data.products) : null;
+  const products = data ? data.products : null;
   useEffect(() => {
     if (researchInput.length >= 3) {
       const inputUpperCase = researchInput.toUpperCase();
@@ -49,16 +77,20 @@ export function Products() {
       );
       setResearchResult(productSorted);
     }
-    if (products && categorie.length >= 1 && researchInput.length < 3) {
+    if (products && categorie.length >= 1 && researchInput.length === 0) {
       const productSorted = products.filter(
         (item) => item.category.name === categorie
       );
       setResearchResult(productSorted);
     }
+    if (products && categorie.length === 0 && researchInput.length === 0) {
+      setResearchResult(products);
+    }
+    console.log(categorie);
   }, [researchInput, categorie, data]);
-
-  console.log(productToOpen);
-
+  const addOnBasket = (productDATA) => {
+    refetch();
+  };
   return (
     <View>
       {productToOpen && openModalProduct && (
@@ -73,8 +105,7 @@ export function Products() {
         >
           <ProductDetails
             addOnBasket={(productDATA) => {
-              console.log(productDATA);
-              refetch();
+              addOnBasket(productDATA);
             }}
             closeProductDetails={() => {
               setProductToOpen(undefined);
@@ -84,23 +115,102 @@ export function Products() {
           />
         </Modal>
       )}
-      <View style={styles.productsResearchContainer}>
-        <Ionicons name="search-outline" />
+      {openCategories && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={openCategories}
+          onRequestClose={() => {
+            setOpenCategories(false);
+          }}
+        >
+          <View style={styles.productsModalCategoriesContainer}>
+            <View style={styles.productsModalCategoriesContainer0} />
+            <View style={styles.productsModalCategories}>
+              <SafeAreaView>
+                <View style={styles.productsModalCategorieButtonContainerFirst}>
+                  <Button
+                    onPress={() => {
+                      setOpenCategories(false);
+                      setCategorie("");
+                    }}
+                    title="Tout"
+                    color="grey"
+                  />
+                </View>
+                <FlatList
+                  data={categories}
+                  scrollEnabled
+                  renderItem={({ item }) => (
+                    <View style={styles.productsModalCategorieButtonContainer}>
+                      <Button
+                        onPress={() => {
+                          setOpenCategories(false);
+                          setCategorie(item.name);
+                        }}
+                        title={item.name}
+                        color="grey"
+                      />
+                    </View>
+                  )}
+                  keyExtractor={(item) => String(item.id)}
+                />
+              </SafeAreaView>
+            </View>
+          </View>
+        </Modal>
+      )}
+      <View
+        style={
+          researchOn
+            ? styles.productsResearchContainerOpen
+            : styles.productsResearchContainerClose
+        }
+      >
+        <Ionicons name="search-outline" style={styles.productsResearchIcon} />
         <TextInput
           placeholder="Rechercher"
           style={styles.productsResearchInput}
           value={researchInput}
           onChangeText={(text) => setResearchInput(text)}
+          onFocus={(text) => setResearchOn(true)}
+          onEndEditing={(e) => setResearchOn(false)}
+          cursorColor="black"
+          autoFocus
         />
+        <View style={styles.productsCategories}>
+          <Button
+            onPress={() => {
+              setOpenCategories(true);
+            }}
+            title={categorie.length === 0 ? "Categories" : categorie}
+            color="grey"
+          />
+        </View>
       </View>
-      <SafeAreaView style={styles.productsResultsContainer}>
+      <SafeAreaView
+        style={
+          researchOn
+            ? styles.productsListContainerResearchOpen
+            : styles.productsListContainerResearchClose
+        }
+      >
         <FlatList
+          style={
+            researchResult.length > 1
+              ? styles.productsListContainerSomeProducts
+              : isKeyboardVisible
+              ? styles.productsListContainerOneProductKeyboard
+              : styles.productsListContainerOneProduct
+          }
           data={researchResult}
+          numColumns={2}
+          scrollEnabled
+          onScroll={() => setResearchOn(false)}
           renderItem={({ item }) => (
             <ProductCard
               addOnBasket={(productDATA) => {
-                console.log(productDATA);
-                refetch();
+                addOnBasket(productDATA);
               }}
               openProduct={(productDATA) => {
                 setProductToOpen(productDATA);
@@ -117,9 +227,85 @@ export function Products() {
 }
 const white = "#ffffff";
 const styles = StyleSheet.create({
-  productsResearchContainer: { backgroundColor: "red" },
-  productsResearchIcon: {},
-  productsResearchInput: {},
-  productsResultsContainer: { backgroundColor: "yellow" },
-  productsResultsItemContainer: {},
+  productsResearchContainerOpen: {
+    backgroundColor: "#EFF0ED",
+    paddingTop: 20,
+    borderTopWidth: 0.5,
+    borderTopColor: "#D9D9D9",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  productsResearchContainerClose: {
+    backgroundColor: "#EFF0ED",
+    borderTopWidth: 0.5,
+    paddingTop: 5,
+    borderTopColor: "#D9D9D9",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  productsResearchIcon: { fontSize: 25, marginHorizontal: 5 },
+  productsResearchInput: { flex: 1 },
+  productsModalCategoriesContainer0: {
+    backgroundColor: "black",
+    opacity: 0.2,
+    position: "absolute",
+    height: "100%",
+    width: width,
+    alignItems: "flex-end",
+  },
+  productsModalCategoriesContainer: {
+    width: width,
+    marginTop: 56,
+    alignItems: "flex-end",
+    height: "100%",
+  },
+  productsModalCategories: {
+    width: 170,
+    paddingHorizontal: 5,
+    backgroundColor: "#EFF0ED",
+  },
+  productsModalCategorieButtonContainerFirst: {
+    marginBottom: 0,
+    height: 40,
+    backgroundColor: "#EFF0ED",
+  },
+  productsModalCategorieButtonContainer: {
+    marginBottom: 5,
+    maxHeight: 50,
+    backgroundColor: "#EFF0ED",
+  },
+  productsCategories: { marginRight: 5 },
+  productsListContainerResearchOpen: {
+    width: width,
+    backgroundColor: "#EFF0ED",
+    paddingBottom: 120,
+    paddingTop: 10,
+    borderTopmWidth: 0.5,
+    borderTopColor: "#D9D9D9",
+  },
+  productsListContainerResearchClose: {
+    width: width,
+    backgroundColor: "#EFF0ED",
+    paddingBottom: 80,
+    paddingTop: 10,
+    borderTopmWidth: 0.5,
+    borderTopColor: "#D9D9D9",
+  },
+  productsListContainerOneProductKeyboard: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+    width: "400%",
+  },
+  productsListContainerOneProduct: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+    width: "200%",
+  },
+  productsListContainerSomeProducts: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+    width: "100%",
+  },
 });
